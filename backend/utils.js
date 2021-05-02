e5c3736c9193985a5ee1cb34cae727315ae0a02c
Petra Munda <petra.munda1@gmail.com>
2021-05-02 09:31:17 +0200

    In app js added a new menu for admin, showing it onli for admin users, in route directory created a new route for admins, adding a midelware in utils in backend  to authenticate only admin users

--- backend/utils.js
+++ backend/utils.js
@@ -45,3 +45,12 @@ export const isAuth = (req, res, next) => {
                 res.status(401).send({ message: 'No Token'});
             }
 };
+
+// function to authenticate onli admin users --> to protect the API
+export const isAdmin = (req, res, next) => {
+    if (req.user && req.user.isAdmin) {
+        next();
+    } else {
+        res.status(401).send({ message: 'Invalid Admin Token'});
+    }
+} 
